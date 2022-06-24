c8a2dfa1157f91c6562f2647ee32f23935394194
idcetim <idcetim@gmail.com>
2022-06-24 15:10:58 +0200

    Change heroku server to Azure server

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,23 +1,23 @@
-export const url = 'https://server-cetim.herokuapp.com/'
+export const url = 'https://server-ecosmartbatt.azurewebsites.net/'
 
-export const urlCelOrigen = 'https://server-cetim.herokuapp.com/celulosa/origen/'
+export const urlCelOrigen = 'https://server-ecosmartbatt.azurewebsites.net/celulosa/origen/'
 
-export const urlCelProceso = 'https://server-cetim.herokuapp.com/celulosa/proceso/'
+export const urlCelProceso = 'https://server-ecosmartbatt.azurewebsites.net/celulosa/proceso/'
 
-export const urlCelProducto = 'https://server-cetim.herokuapp.com/celulosa/producto'
+export const urlCelProducto = 'https://server-ecosmartbatt.azurewebsites.net/celulosa/producto'
 
-export const urlCelConsultaLotes = 'https://server-cetim.herokuapp.com/celulosa/consulta/'
+export const urlCelConsultaLotes = 'https://server-ecosmartbatt.azurewebsites.net/celulosa/consulta/'
 
-export const urlCelOrigenHash = 'https://server-cetim.herokuapp.com/celulosa/hash/origen/'
+export const urlCelOrigenHash = 'https://server-ecosmartbatt.azurewebsites.net/celulosa/hash/origen/'
 
-export const urlCelProcesosHash = 'https://server-cetim.herokuapp.com/celulosa/hash/procesos/'
+export const urlCelProcesosHash = 'https://server-ecosmartbatt.azurewebsites.net/celulosa/hash/procesos/'
 
-export const urlCelProductoHash = 'https://server-cetim.herokuapp.com/celulosa/hash/producto/'
+export const urlCelProductoHash = 'https://server-ecosmartbatt.azurewebsites.net/celulosa/hash/producto/'
 
-export const urlSilLotes = 'https://server-cetim.herokuapp.com/silicio/lotes/'
+export const urlSilLotes = 'https://server-ecosmartbatt.azurewebsites.net/silicio/lotes/'
 
-export const urlSilProducto = 'https://server-cetim.herokuapp.com/silicio/producto/'
+export const urlSilProducto = 'https://server-ecosmartbatt.azurewebsites.net/silicio/producto/'
 
-export const urlSilLotesHash = 'https://server-cetim.herokuapp.com/silicio/hash/lotes/'
+export const urlSilLotesHash = 'https://server-ecosmartbatt.azurewebsites.net/silicio/hash/lotes/'
 
-export const urlSilProductoHash = 'https://server-cetim.herokuapp.com/silicio/hash/producto/'
+export const urlSilProductoHash = 'https://server-ecosmartbatt.azurewebsites.net/silicio/hash/producto/'
