b68a49b4b28e79a0fdcd3b4173d9db0e232e7f1e
idcetim <idcetim@gmail.com>
2022-10-29 16:29:58 +0200

    Add photo background to all the application

--- src/App.js
+++ src/App.js
@@ -17,30 +17,32 @@ import { VerStockEntradas } from "./routes/silicio/VerStockEntradas";
 import { VerStockProduccion } from "./routes/silicio/VerStockProduccion";
 
 function App() {
-    return (<div >
+    return (<div id="home">
         <BrowserRouter >
-            <NavBar />
-            <Routes >
-                <Route path="/" element={< Home />} />
-                <Route path="/celulosa" element={< HomeCelulosa />} />
-                <Route path="/celulosa/origen/*" element={< Origen />} />
-                <Route path="/celulosa/procesos/*" element={< Procesos />} />
-                <Route path="/celulosa/producto/*" element={< ProductoFinal />} />
-                {/* <Route path="/celulosa/consulta/*" element={< ConsultarCelulosa />} /> */}
-                <Route path="/celulosa/consulta/lotes/" element={< ConsultarLotesCel />} />
-                <Route path="/silicio" element={< HomeSilicio />} />
-                <Route path="/silicio/entradas/*" element={< Entradas />} />
-                <Route path="/silicio/entradas/analisis/*" element={< AddAnalisisEntrada />} />
-                <Route path="/silicio/produccion/*" element={< Produccion />} />
-                <Route path="/silicio/produccion/analisis/*" element={< AddAnalisisProduct />} />
-                <Route path="/silicio/verentradas" element={< VerStockEntradas />} />
-                <Route path="/silicio/verproduccion" element={< VerStockProduccion />} />
-                {/* <Route path="/silicio/consulta/*" element={< ConsultarLoteSilicio />} />
+            <div >
+                <NavBar />
+                <Routes >
+                    <Route path="/" element={< Home />} />
+                    <Route path="/celulosa" element={< HomeCelulosa />} />
+                    <Route path="/celulosa/origen/*" element={< Origen />} />
+                    <Route path="/celulosa/procesos/*" element={< Procesos />} />
+                    <Route path="/celulosa/producto/*" element={< ProductoFinal />} />
+                    {/* <Route path="/celulosa/consulta/*" element={< ConsultarCelulosa />} /> */}
+                    <Route path="/celulosa/consulta/lotes/" element={< ConsultarLotesCel />} />
+                    <Route path="/silicio" element={< HomeSilicio />} />
+                    <Route path="/silicio/entradas/*" element={< Entradas />} />
+                    <Route path="/silicio/entradas/analisis/*" element={< AddAnalisisEntrada />} />
+                    <Route path="/silicio/produccion/*" element={< Produccion />} />
+                    <Route path="/silicio/produccion/analisis/*" element={< AddAnalisisProduct />} />
+                    <Route path="/silicio/verentradas" element={< VerStockEntradas />} />
+                    <Route path="/silicio/verproduccion" element={< VerStockProduccion />} />
+                    {/* <Route path="/silicio/consulta/*" element={< ConsultarLoteSilicio />} />
                 <Route path="/silicio/consulta/lotes/*" element={< ConsultarTodosSilicio />} /> */}
-                <Route path="/baterias" element={< HomeBaterias />} />
-                <Route path="/consultar" element={< Consultar />} />
+                    <Route path="/baterias" element={< HomeBaterias />} />
+                    <Route path="/consultar" element={< Consultar />} />
 
-            </Routes>
+                </Routes>
+            </div>
         </BrowserRouter>
     </div>
     );
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,29 +5,27 @@ import "../styles/global.css"
 import "../styles/modal.css"
 import gain from '../data/gain.png'
 const Home = () => {
-    const [openBlockchainModal, setOpenBlockchainModal] = useState(false)
-    const [openProyectModal, setOpenProyectModal] = useState(false)
-    const gainUrl ="http://gain.xunta.gal/?locale=es_ES"
-    return (
-        <div className="web-wrapper">
-
-            <h1 className="main-h1"> Proyecto ECO-SMART-BATT</h1>
-           <a href={gainUrl} target={"_blank"} rel={"noreferrer"}> <img src={gain} alt={"This in gain"} className="landing-image"/></a>
-            <div className="modalPosition">
-                {openBlockchainModal && <BlockchainModal closeModal={setOpenBlockchainModal} />}
-            </div>
-            <div className="modalPosition">
-                {openProyectModal && <ProyectInfoModal closeModal={setOpenProyectModal} />}
-            </div>
-            <div className="small-separation">
-                <button className="button-home" onClick={() => setOpenProyectModal(true)}>Información del proyecto</button>
-            </div>
-            <div className="small-separation">
-                <button className="button-home" onClick={() => setOpenBlockchainModal(true)}>Información blockchain</button>
-            </div>
-
+  const [openBlockchainModal, setOpenBlockchainModal] = useState(false)
+  const [openProyectModal, setOpenProyectModal] = useState(false)
+  const gainUrl = "http://gain.xunta.gal/?locale=es_ES"
+  return (
+      <section>
+        <h1 className="main-h1"> Proyecto ECO-SMART-BATT</h1>
+        <a href={gainUrl} target={"_blank"} rel={"noreferrer"}> <img src={gain} alt={"This in gain"} className="landing-image" /></a>
+        <div className="modalPosition">
+          {openBlockchainModal && <BlockchainModal closeModal={setOpenBlockchainModal} />}
         </div>
-    )
+        <div className="modalPosition">
+          {openProyectModal && <ProyectInfoModal closeModal={setOpenProyectModal} />}
+        </div>
+        <div className="small-separation">
+          <button className="button-home" onClick={() => setOpenProyectModal(true)}>Información del proyecto</button>
+        </div>
+        <div className="small-separation">
+          <button className="button-home" onClick={() => setOpenBlockchainModal(true)}>Información blockchain</button>
+        </div>
+      </section>
+  )
 }
 
 export default Home;
